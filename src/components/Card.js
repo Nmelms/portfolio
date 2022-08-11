bcd840df48c27770195d68fcac5fcfef3f628773
Nick Melms <nmelms92@gmail.com>
2022-08-10 23:43:28 -0400

    added github links

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,20 +5,25 @@ import { faGithub } from "@fortawesome/fontawesome-free-brands";
 
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
-export default function card({ img, text, liveLink }) {
+export default function card({ img, text, liveLink, title, githubLink }) {
   return (
     <div className="card">
+      <h2 className="cardTitle">{title}</h2>
       <img className="cardImg" src={img} />
 
       {text}
-      <a className="linkIcon" href={liveLink}>
-        <FontAwesomeIcon
-          className="faLinkIcon"
-          size="2x"
-          icon={faArrowUpRightFromSquare}
-        />
-      </a>
-      <FontAwesomeIcon className="faLinkIcon" size="2x" icon={faGithub} />
+      <div className="icons">
+        <a className="linkIcon" href={liveLink}>
+          <FontAwesomeIcon
+            className="faLinkIcon"
+            size="2x"
+            icon={faArrowUpRightFromSquare}
+          />
+        </a>
+        <a className="linkIcon" href={githubLink}>
+          <FontAwesomeIcon className="faLinkIcon" size="2x" icon={faGithub} />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Card from "./Card";
 import invoiceScreenshot from "../assets/invoiceScreenshot.png";
+import planetScreenshot from "../assets/planetScreenshot.png";
 
 export default function Project() {
   return (
     <div className="projects">
       <h1 className="projectsTitle">Projects</h1>
+
       <Card
+        title="Invoice App"
         liveLink="https://glittery-shortbread-a5497a.netlify.app/"
-        text="this is a bunch of text im probably going to change at some point"
+        text="This CRUD app was built using React and firebase as a backend"
+        githubLink="https://github.com/Nmelms/Invoice-App"
         img={invoiceScreenshot}
+      />
+      <Card
+        title="Planet Facts"
+        liveLink="https://dainty-bombolone-d66269.netlify.app/"
+        githubLink="https://github.com/Nmelms/planets"
+        text="Planet facts site was built using React"
+        img={planetScreenshot}
       />
     </div>
   );
